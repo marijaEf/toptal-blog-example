516f0155d0f574fc9878922a5fc35a70f313c242
Marija Efremova <marija@toptal.com>
2015-02-28 23:11:51 +0100

    Initial Commit

---
+++ BlogExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogExample
+//
+//  Created by Marija Efremova on 2/28/15.
+//  Copyright (c) 2015 Marija Efremova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogExample
+//
+//  Created by Marija Efremova on 2/28/15.
+//  Copyright (c) 2015 Marija Efremova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
